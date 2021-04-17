0b0d4d3c796fa79f410e4f4dac9f7a83b3816bbd
hao619 <332715766@qq.com>
2021-04-17 14:01:31 +0800

    add

--- avsn/avsn/items.py
+++ avsn/avsn/items.py
@@ -8,5 +8,25 @@ import scrapy
 
 class AvsnItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    avsn = scrapy.Field()
+    av_tittle = scrapy.Field()
+    av_description = scrapy.Field()
+    # 
+    av_tags = scrapy.Field()
+    # 演员
+    av_performers = scrapy.Field()
+    # 分类
+    av_classifys = scrapy.Field()
+    # 评分
+    score = scrapy.Field()
+    # 开发商 发行商
+    publisher = scrapy.Field()
+    # 生产者
+    producers = scrapy.Field()
+    # 导演
+    director = scrapy.Field()
+    # 时长
+    length = scrapy.Field()
+    # 发行时间
+    issue_date = scrapy.Field()
     pass
